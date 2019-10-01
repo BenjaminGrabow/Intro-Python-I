d1963eb578413561c9fd15d4b9b51193803dbef8
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-01 22:05:23 +0200

    bugfix in file 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,7 +9,7 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
-file = open("src/foo.txt", "r")
+file = open("./foo.txt", "r")
 content = file.read()
 print(content)
 file.close()
@@ -20,16 +20,16 @@ file.close()
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
-a = ["""Open up a file called bar.txt (which doesn't exist yet) for
+text = ["""Open up a file called bar.txt (which doesn't exist yet) for
 writing. Write three lines of arbitrary content to that file, then close the file. Open up "bar.txt" and inspect it to make 
  sure that it contains what you expect it to contain"""]
 # YOUR CODE HERE
-file= open("src/bar.txt","w+")
-for i in a:
+file= open("./bar.txt","w+")
+for i in text:
     file.write(i+"\n")
 file.close()
 
-file = open("src/bar.txt", "r")
+file = open("./bar.txt", "r")
 content = file.read()
 print("\n")
 print(content)
